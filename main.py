d0d7453c8e3e4a72add0cd4ae2822c588c51df4d
GrzegorzSek <241778@student.pwr.edu.pl>
2021-05-03 20:53:09 +0200

    wyniki-z-lambda

--- main.py
+++ main.py
@@ -4,20 +4,25 @@
 
 # Imports
 import numpy as np
+from prettytable import PrettyTable
 
 
 def main():
-    a = np.array([[0., 1., 1.], [-8., -1., -2.], [-6., -2., -1.], [-5., -1., -1.]]) # wiele rozwiązań na zbiorze ograniczonym
+    a = np.array(
+        [[0., 1., 1.], [-8., -1., -2.], [-6., -2., -1.], [-5., -1., -1.]])  # wiele rozwiązań na zbiorze ograniczonym
     # a = np.array([[0., 0.5, 1.], [0., -1., 1.], [5., 1., 1.]])    # Tylko jedno rozwiązanie
 
+    dim = 2     # wymiar zadania
     a_dict = {}
     a_dict2 = {}
+    a_dict3 = {}
     a_goal = [0, 1, 2]
-    a_support = [0, 3, 4, 5]    # zmienne pomocnicze
+    a_support = [0, 3, 4, 5]  # zmienne pomocnicze
     rows: int = a.shape[0]  # liczba wierszy
     cols: int = a.shape[1]  # liczba kolumn
     # print(rows, cols)
-    step_counter = 1
+    step_counter = 1    # liczy kroki - kolejne tabele simpleksowe
+    bounded_solution = np.zeros((dim, dim))     # tabela do wyniku wielu rozw. na zbiorze ogr.
 
     is_a = is_acceptable(cols, a)
 
@@ -37,7 +42,28 @@ def main():
 
             is_b = is_optimal(rows, a)
             step_counter += 1
+
+            print("macierz wyników")
             # print(a)
+            print_solution(a, rows, cols, a_goal, a_support)
+            print()
+
+            print("tabele pomocnicze")
+            print("f celu: ")
+            print(a_goal)
+            print("zm pomocnicze: ")
+            print(a_support)
+            print()
+
+            print("wynik jako dictionary")
+            answer_dict(a, a_goal, a_support, a_dict3)
+            print(a_dict3)
+
+            print("wynik jako wektor")
+            ans3 = []
+            answer_array(a_dict3, ans3)
+            print(ans3)
+            print()
 
         print("macierz wyników")
         print(a)
@@ -60,67 +86,47 @@ def main():
         print(ans1)
         print()
 
-        # po rozwiązaniu sprawdzamy czy ma zero w funckji celu i wykonujemy odpowiednie akcje
-        # has_inf_solutions = is_multiple_solutions(a, cols)
-        # if has_inf_solutions:
-        #     col_no = col_to_opt(a, cols)
-        #     row_no = row_to_simplex(a, rows, col_no)
-        #     a = gaussian_elimination(a, row_no, col_no, rows, cols)
-        #     swap_x(a_goal, a_support, row_no, col_no)
-        #
-        #     print("macierz wyników")
-        #     print(a)
-        #     print()
-        #
-        #     print("tabele pomocnicze")
-        #     print("f celu: ")
-        #     print(a_goal)
-        #     print("zm pomocnicze: ")
-        #     print(a_support)
-        #     print()
-        #
-        #     print("wynik jako dictionary")
-        #     answer_dict(a, a_goal, a_support, a_dict2)
-        #     print(a_dict2)
-        #
-        #     print("wynik jako wektor")
-        #     ans2 = []
-        #     answer_array(a_dict2, ans2)
-        #     print(ans2)
-        #     print()
-
         i_s_c = inf_solutions_condition(a, cols)
-        if i_s_c:   # Zadanie spełnia warunki na nieskończenie wiele rozwiązań?
+        if i_s_c:  # Zadanie spełnia warunki na nieskończenie wiele rozwiązań?
             on_limited_set = is_on_limited_set(a, rows, cols)
             on_unlimited_set = is_on_unlimited_set(a, rows, cols)
             if on_limited_set != 0:
-                print('Zadanie posiada wiele rozwiązań na zbiorze ograniczonym')
-                print()
-                col_no = col_to_opt(a, cols)
-                row_no = row_to_simplex(a, rows, col_no)
-                a = gaussian_elimination(a, row_no, col_no, rows, cols)
-                swap_x(a_goal, a_support, row_no, col_no)
+                print('A_DICT: ')
+                print(a_dict)
+                bounded_solution[0, 0] = a_dict[1]
+                bounded_solution[0, 1] = a_dict[2]
+                for d in range(1, dim):  # pętla, bo musi przeliczyć tyle razy ile ma wymiar zadania
+                    print('Zadanie posiada nieskończenie wiele rozwiązań na zbiorze ograniczonym')
+                    print()
+                    col_no = col_to_opt(a, cols)
+                    row_no = row_to_simplex(a, rows, col_no)
+                    a = gaussian_elimination(a, row_no, col_no, rows, cols)
+                    swap_x(a_goal, a_support, row_no, col_no)
 
-                print("macierz wyników")
-                print(a)
-                print()
+                    # print("macierz wyników")
+                    # print(a)
+                    # print()
+                    #
+                    # print("tabele pomocnicze")
+                    # print("f celu: ")
+                    # print(a_goal)
+                    # print("zm pomocnicze: ")
+                    # print(a_support)
+                    # print()
+                    #
+                    print("wynik jako dictionary")
+                    answer_dict(a, a_goal, a_support, a_dict2)
+                    print(a_dict2)
 
-                print("tabele pomocnicze")
-                print("f celu: ")
-                print(a_goal)
-                print("zm pomocnicze: ")
-                print(a_support)
-                print()
+                    print("wynik jako wektor")
+                    ans2 = []
+                    answer_array(a_dict2, ans2)
+                    print(ans2)
+                    print()
 
-                print("wynik jako dictionary")
-                answer_dict(a, a_goal, a_support, a_dict2)
-                print(a_dict2)
-
-                print("wynik jako wektor")
-                ans2 = []
-                answer_array(a_dict2, ans2)
-                print(ans2)
-                print()
+                    bounded_solution[d, 0] = a_dict2[1]
+                    bounded_solution[d, 1] = a_dict2[2]
+                print_bounded_solution(bounded_solution)
             elif on_unlimited_set != 0:
                 print('Zadanie posiada wiele rozwiązań na zbiorze nieograniczonym')
             else:
@@ -136,22 +142,42 @@ def main():
         print('Rozwiązanie nie jest dualnie dopuszczalne')
 
 
-def inf_solutions_condition(a, cols):    # sprawdza czy zadanie spełnia warunki na nieskończenie wiele rozwiązań
-    for j in range(1, cols):    # jest to warunek y_0 j >= 0
+def print_bounded_solution(bounded_solution):
+    dim = bounded_solution.shape[0]
+    print('Rozwiązanie zdania dla nieskończenie wielu rozwiązań na zbiorze ograniczonym: ')
+    for d in range(0, dim):
+        if d < dim - 1:
+            print(bounded_solution[d], end="")
+            print('*\u03BB' + str(d+1) + ' + ', end="")
+        else:
+            print(bounded_solution[d], end="")
+            print('*\u03BB' + str(d+1))
+
+
+def print_solution(a, rows, cols, a_goal, a_support):
+    s = PrettyTable(['X', str(a_goal)])
+
+    for i in range(0, rows):
+        s.add_row([str(a_support[i]), a[0]])
+    print(s)
+
+
+def inf_solutions_condition(a, cols):  # sprawdza czy zadanie spełnia warunki na nieskończenie wiele rozwiązań
+    for j in range(1, cols):  # jest to warunek y_0 j >= 0
         if a[0, j] < 0:
             return False
         return True
 
 
-def is_on_limited_set(a, rows, cols):   # sprawdza czy zadanie posiada nieskończenie wiele rozwiązań na zb. ogr.
+def is_on_limited_set(a, rows, cols):  # sprawdza czy zadanie posiada nieskończenie wiele rozwiązań na zb. ogr.
     col = 0
-    for j in range(1, cols):    # sprawdza czy w wierszu występuje zero - warunek: y_0 j_0 = 0
+    for j in range(1, cols):  # sprawdza czy w wierszu występuje zero - warunek: y_0 j_0 = 0
         if a[0, j] == 0:
             col = j
         else:
             return col
 
-    for i in range(1, rows):    # sprawdza kolejne dwa warunki y_i_0 0 > 0 oraz y_i_0 j_0 >0
+    for i in range(1, rows):  # sprawdza kolejne dwa warunki y_i_0 0 > 0 oraz y_i_0 j_0 >0
         if a[i, 0] > 0:
             if a[i, col] > 0:
                 return col
@@ -162,16 +188,16 @@ def is_on_limited_set(a, rows, cols):   # sprawdza czy zadanie posiada nieskońc
     return col
 
 
-def is_on_unlimited_set(a, rows, cols): # spradza czy zadanie ma wiele rozw. na zb. nieogr.
+def is_on_unlimited_set(a, rows, cols):  # spradza czy zadanie ma wiele rozw. na zb. nieogr.
     row = 0  # zmienna licząca wiersze z degeneracją
     col = 0
-    for j in range(1, cols):    # sprawdza czy w wierszu występuje zero - warunek: y_0 j_0 = 0
+    for j in range(1, cols):  # sprawdza czy w wierszu występuje zero - warunek: y_0 j_0 = 0
         if a[0, j] == 0:
             col = j
         else:
             return col
 
-    for i in range(1, rows):    # sprawdza czy w zadaniu występuje degeneracja - warunek y_i0 = 0 dla i=1, ..., m
+    for i in range(1, rows):  # sprawdza czy w zadaniu występuje degeneracja - warunek y_i0 = 0 dla i=1, ..., m
         if a[i, 0] == 0:
             row = row + 1
 
@@ -179,14 +205,14 @@ def is_on_unlimited_set(a, rows, cols): # spradza czy zadanie ma wiele rozw. na 
         print('Zachodzi degeneracja')
         return col
 
-    for i in range(1, rows):   # sprawdza warunek: y_i j_0 <= 0 dla i=1, ..., m
+    for i in range(1, rows):  # sprawdza warunek: y_i j_0 <= 0 dla i=1, ..., m
         if a[i, col] > 0:
             return 0
 
     return col
 
 
-def is_on_unlimited_task(a, rows, cols): # sprawdza czy zadanie jest nieograniczone
+def is_on_unlimited_task(a, rows, cols):  # sprawdza czy zadanie jest nieograniczone
     for j in range(0, cols):
         if a[0, j] < 0:
             for i in range(1, rows):
@@ -204,7 +230,7 @@ def answer_dict(a, a_goal, a_support, a_dict):
 
 
 def answer_array(a_dict, ans):
-    for i in range(1, len(a_dict)+1):
+    for i in range(1, len(a_dict) + 1):
         ans.append(a_dict.get(i))
 
 
@@ -234,7 +260,7 @@ def is_optimal(rows, a):  # test optymalności zaczyna się od wiersza 1 nie od 
 #             return True
 
 
-def col_to_opt(a, cols):  # bierzemy kolumnę dla której jest w pierwszym wierszu jest zero
+def col_to_opt(a, cols):  # bierzemy kolumnę, dla której w pierwszym wierszu jest zero
     for j in range(1, cols):
         if a[0, j] == 0:
             return j
@@ -269,7 +295,8 @@ def variable_to_remove(rows, a):
         if x > a[i + 1, 0]:
             x = a[i + 1, 0]
             row += 1
-    # print(x)
+    print('Usuwamy wiersz: ' + str(row))
+    print(x)
     return row
 
 
@@ -278,19 +305,21 @@ def variable_to_add(cols, a, row):
     x = 0
     col = 1
 
-    for i in range(1, cols):
-        if a[row, i] < 0:
-            x = a[0, 1] / a[row, i]
+    for j in range(1, cols):  # tutaj bierze pierwszą napotkaną wartość y_0j/y_rj < 0
+        if a[row, j] < 0:
+            x = a[0, 1] / a[row, j]
             # print(x)
             new_starting_point += 1
-            # print(new_starting_point)
+            print('Nowy punkt startowy to: ')
+            print(new_starting_point)
             break
 
-    for i in range(new_starting_point, cols - 1):
-        if a[row, i + 1] < 0 and a[0, i] / a[row, i + 1] > x:
-            x = a[0, i] / a[row, i + 1]
+    for j in range(new_starting_point, cols - 1):
+        if a[row, j + 1] < 0 and a[0, j] / a[row, j + 1] > x:
+            x = a[0, j] / a[row, j + 1]
             col += 1
-    # print(col)
+    print('Dodajemy kolumnę: ' + str(col))
+    print(x)
     return col
 
 
